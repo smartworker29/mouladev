56eaf2c9b42d4494246c10df8ef59d46c6e175cd
soliddev312 <hitgoal312@gmail.com>
2016-03-29 03:00:11 -0500

    Set up environment
    Signed-off-by: soliddev312 <hitgoal312@gmail.com>

---
+++ src/environments/enviroment.test.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  server: 'https://saffrondab.herokuapp.com/api/v1',
+  oauth_redirect_uri: 'https://saffrondab.herokuapp.com/'
+};
---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  server: 'https://saffrondab-stage.herokuapp.com/api/v1',
+  oauth_redirect_uri: 'https://saffrondab-stage.herokuapp.com/'
+};
